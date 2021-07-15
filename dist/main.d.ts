26ecc2ed7620481394962d95dd517b100d66cb28
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-15 14:17:52 +0700

    them vi du 2

---
+++ dist/main.d.ts
@@ -0,0 +1,6 @@
+import set = Reflect.set;
+declare let money: number;
+declare const buyACar: (car: any) => Promise<unknown>;
+declare const promise: Promise<void>;
+declare function handleTimeout(timeout: any): Promise<unknown>;
+declare var xxx: Promise<unknown>;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import set = Reflect.set;
+
 let money = 10000;
 const buyACar = (car: any) => {
     return new Promise(((resolve, reject) => {
@@ -17,3 +19,26 @@ const promise = buyACar("Vinfast").then(value => {
 }, error => {
     console.log(error);
 })
+
+function handleTimeout(timeout:any){
+    return new Promise(function(resolve,reject){
+        setTimeout(resolve,timeout);
+    });
+}
+
+var xxx= handleTimeout(5000);
+xxx.then(function (){
+    console.log("Lập");
+})
+.then(function(){
+    return handleTimeout(3000);
+})
+.then(function(){
+    console.log("Trình");
+})
+.then(function(){
+    return handleTimeout(2000);
+})
+.then(function(){
+    console.log("JaVa");
+});
