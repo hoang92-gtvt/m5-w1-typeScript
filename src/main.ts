d53cd5f689be03905b9e00e359a6b5ceb66cb0f7
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-15 13:56:04 +0700

    demo with promise

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-import {Rectangle} from "./rectangle";
+let money = 10000;
+const buyACar = (car: any) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve("can buy " + car);
+            } else {
+                reject("Do not enough money");
+            }
+        }, 100);
+    }))
+}
 
-const rectangle = new Rectangle(5, 5, 10, 20);
-console.log(rectangle.toString());
-console.log(rectangle.area());
+money = 8000;
+const promise = buyACar("Vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+})
